b9d673612ec52af4013db34465fc581a93d5cb60
Daniel Mattox <dmattox10@gmail.com>
2020-05-05 00:33:05 -0400

    Moving between rooms works!

--- src/adv.py
+++ src/adv.py
@@ -1,22 +1,22 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
+    'outside':  Room('outside', "Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer':    Room('foyer', "Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room('overlook', "Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':   Room('narrow', "Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room('treasure', "Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -49,3 +49,37 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+name = input('Enter your name... ')
+player = Player(name, room['outside'])
+print('[q] to quit, [n,e,s,w] to move, [take $ITEM] to take, [drop $ITEM] to drop')
+while True:
+    output = player.Surroundings()
+    print(f'{output}')
+
+    cmd = input(f'{player}, enter a command... ').split(' ')
+
+    if len(cmd) == 1:
+        if cmd[0] == 'q':
+            exit(0)
+        if cmd[0] == 'n':
+            if 'n' in room[Player.Location(player)].Doors():
+                player.Move(room[Player.Location(player)].n_to)
+        if cmd[0] == 'e':
+            if 'e' in room[Player.Location(player)].Doors():
+                player.Move(room[Player.Location(player)].e_to)
+        if cmd[0] == 's':
+            if 's' in room[Player.Location(player)].Doors():
+                player.Move(room[Player.Location(player)].s_to)
+        if cmd[0] == 'w':
+            if 'w' in room[Player.Location(player)].Doors():
+                player.Move(room[Player.Location(player)].w_to)
+        else:
+            pass
+
+    if len(cmd) == 2:
+        item = cmd[1]
+        if cmd[0] == 'take':
+            pass
+        if cmd[0] == 'drop':
+            pass
--- src/room.py
+++ src/room.py
@@ -1,8 +1,29 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, location, description, n_to=None, e_to=None, s_to=None, w_to=None):
         self.name = name
         self.description = description
+        self.n_to = n_to
+        self.e_to = e_to
+        self.s_to = s_to
+        self.w_to = w_to
+        self.doors = []
+        
+    def Doors(self):
+        if self.n_to is not None:
+            self.doors.append('n')
+            print(f'Room {self.name} has a door to the N')
+        if self.e_to is not None:
+            self.doors.append('e')
+            print(f'Room {self.name} has a door to the E')
+        if self.s_to is not None:
+            self.doors.append('s')
+            print(f'Room {self.name} has a door to the S')
+        if self.w_to is not None:
+            self.doors.append('w')
+            print(f'Room {self.name} has a door to the W')
+        return self.doors
+
     def __str__(self):
-        return f'name {name}, description {description}'
+        return f'current location: {self.name}, {self.description}'
